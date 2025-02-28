b4402a3f62b69076e4e671bc1f6959bf71763c0d
Vansh5632 <vanshgilhotra8885@gmail.com>
2025-02-28 23:01:37 +0530

    sigin and signup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import {middleware} from './middleware';
+import JWT_SECRET from './config';
 
 const app = express();
 
+app.post('/login',(req,res)=>{
+
+});
+
+app.post('/register',(req,res)=>{
+
+});
+app.post('/create-room',middleware,(req,res)=>{
+
+})l
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
